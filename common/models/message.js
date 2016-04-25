0d3eb1f70ae4c08af6eed1acb69b52d00b5993b6
bfilia <brett.com@gmail.com>
2016-04-25 18:04:13 -0400

    added palindrome check

--- common/models/message.js
+++ common/models/message.js
@@ -1,3 +1,20 @@
-module.exports = function(Message) {
+var isPalindrome = require('is-palindrome');
 
+module.exports = function(Message) {
+  Message.palindromeCheck = function(mID, cb) {
+    Message.findById( mID, function (err, instance) {
+        var pal = isPalindrome(instance.message);
+        response = pal;
+        cb(null, response);
+        console.log(response);
+    });
+  };
+  Message.remoteMethod (
+      'palindromeCheck',
+      {
+        http: {path: '/palindromecheck', verb: 'get'},
+        accepts: {arg: 'id', type: 'string', http: { source: 'query' } },
+        returns: {arg: 'palindrome', type: 'Boolean'}
+      }
+  );
 };
