303c2aaaeea13f86e1b044be379aa80bbd779081
bfilia <brett.com@gmail.com>
2016-04-25 18:10:15 -0400

    small tweak

--- common/models/message.js
+++ common/models/message.js
@@ -3,8 +3,7 @@ var isPalindrome = require('is-palindrome');
 module.exports = function(Message) {
   Message.palindromeCheck = function(mID, cb) {
     Message.findById( mID, function (err, instance) {
-        var pal = isPalindrome(instance.message);
-        response = pal;
+        response = isPalindrome(instance.message);
         cb(null, response);
         console.log(response);
     });
